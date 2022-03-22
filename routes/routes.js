83ef9ec5f1a5c7bee55d6107f565bd1e66b9f4a7
jesus-coder-code <jesusdeavila2001@gmail.com>
2022-03-22 15:11:52 -0500

    añadiendo validaciones en post y put y metodo get para buscar por numero

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const routes = express.Router(); //para manejar las rutas de los metodos
+const { check, validationResult } = require("express-validator");
 
 //haremos uso del modulo Contact del archivo database.js para nuestros metodos
 const { Contact } = require("../database");
@@ -10,18 +11,52 @@ routes.get("/", async (req, res) => {
   res.json(contacts);
 });
 
-//metodo post para crear nuevos contactos
-routes.post("/", async (req, res) => {
-  await Contact.create(req.body);
-  res.json({ message: "contact created" });
+//obtener los contactos por numero de telefono
+routes.get("/:number", async (req, res) => {
+  const contacts = await Contact.findAll({
+    where: { number: req.params.number },
+  });
+  res.json(contacts);
 });
+
+//metodo post para crear nuevos contactos
+routes.post(
+  "/",
+  [
+    check("name", "este campo no debe estar vacio").not().isEmpty(),
+    check("number", "este campo es de solo numeros").isNumeric(),
+    check("email", "esto no es un email").isEmail(),
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(422).json({ message: error.array() });
+    }
+
+    await Contact.create(req.body);
+    res.json({ message: "contact created" });
+  }
+);
 
 /*el metodo put recibe por parámetros
 el id del contacto que queremos modificar*/
-routes.put("/:id", async (req, res) => {
-  await Contact.update(req.body, { where: { id: req.params.id } });
-  res.json({ message: "contact updated" });
-});
+routes.put(
+  "/:id",
+  [
+    check("name", "este campo no debe estar vacio").not().isEmpty(),
+    check("number", "este campo es de solo numeros").isNumeric(),
+    check("email", "esto no es un email").isEmail(),
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(422).json({ message: error.array() });
+    }
+
+    await Contact.update(req.body, { where: { id: req.params.id } });
+    res.json({ message: "contact updated" });
+  }
+);
 
 /*el metodo delete recibe por parámetros
 el id del contacto que queremos eliminar*/
